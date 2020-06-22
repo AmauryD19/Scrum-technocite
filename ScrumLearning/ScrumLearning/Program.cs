8bf56132c7684ed06af005136dfad040b5aabc34
factoone <adrien.denis03@gmail.com>
2020-06-22 16:12:07 +0200

    us 5 : j'ai recupéré la température a bruxelles pour l'afficher

--- ScrumLearning/ScrumLearning/Program.cs
+++ ScrumLearning/ScrumLearning/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+using static ScrumLearning.Meteo;
 
 namespace ScrumLearning
 {
     class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
+            Meteo meteo = new Meteo();
+            meteo.RequestWeatherAsync();
+            Console.WriteLine("zeffdez");
+            Console.ReadKey();
         }
     }
 }
