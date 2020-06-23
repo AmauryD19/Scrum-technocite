9c974407311ba880c84861f69f6dc9a0cfc6cd02
ThibaultD25 <64505837+ThibaultD25@users.noreply.github.com>
2020-06-23 11:24:10 +0200

    Merge Update

--- ScrumLearning/ScrumLearning/Menu.cs
+++ ScrumLearning/ScrumLearning/Menu.cs
@@ -53,7 +53,7 @@ namespace ScrumLearning
                     case 1: SerializationOpinion.AddOpinion(); ShowMenu(MenuType.MainMenu, true, 0); break;
                     case 2: SerializationOpinion.ShowOpinion(); ShowMenu(MenuType.MainMenu, true, 0); break;
                     case 3: /*BXL_METEO */; ShowMenu(MenuType.MainMenu, true, 0); break;
-                    case 4: /* CALCULATOR */; ShowMenu(MenuType.MainMenu, true, 0); break;
+                    case 4: Calculator.CalculatorMethod(); ShowMenu(MenuType.MainMenu, true, 0); break;
                     case 5:
                         Environment.Exit(0);
                         break;
